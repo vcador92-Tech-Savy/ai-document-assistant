6d97acb1c6dfdc4642ee1799f4895bd1f32c44e1
Venix Cador <venixcador@Richards-MacBook-Pro.local>
2026-02-26 00:31:49 -0500

    Day 1: setup solution + enable Swagger

---
+++ Api/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
